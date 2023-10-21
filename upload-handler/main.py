3673bbb9cdd50ba7bfe3720716ce54b8ed45d347
Maximilian <66524685+maxbirnbacher@users.noreply.github.com>
2023-10-21 13:09:55 +0200

    added Request import

--- upload-handler/main.py
+++ upload-handler/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Request
 from gridfs import GridFS
 from pymongo import MongoClient
 from pydantic import BaseModel
