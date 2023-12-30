f608cc2b660a815dd4689a15a01f0dcaab41d0dd
Maximilian <66524685+maxbirnbacher@users.noreply.github.com>
2023-12-30 09:35:32 +0100

    added new styling for html

--- horizon-frontend/routes/command-center.js
+++ horizon-frontend/routes/command-center.js
@@ -4,7 +4,13 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     // TODO: Add call to message queue to get the list of reverse shells
-    res.render('command-center');
+
+    //make a call to the api to get the list of reverse shells
+    let connection_list = fetch('http://localhost:3000/api/connections')
+        .then(res => res.json())
+        .then(json => console.log(json));
+
+    res.render('command-center', { connection_list: ['shell1', 'shell2', 'shell3'] });
 });
 
 /* GET reverse shell terminal */
