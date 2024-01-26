adb4bd4ccd01aeaadbf3b4d727a78ab7f2eabf2a
Maximilian <66524685+maxbirnbacher@users.noreply.github.com>
2024-01-26 22:18:59 +0100

    changed debug message

--- c2-api/main.py
+++ c2-api/main.py
@@ -39,11 +39,11 @@ async def register_connection(request: Request):
     # check if the connection has the required fields
     if 'ip_address' not in data or 'hostname' not in data or 'username' not in data or 'os' not in data:
         raise HTTPException(status_code=400, detail="Missing required fields")
-
-    print(data.ip_address)
-    print(data.hostname)
-    print(data.username)
-    print(data.os)
+    
+    print(data[ip_address])
+    print(data[hostname])
+    print(data[username])
+    print(data[os])
 
     # Add the timestamp to the connection
     data['timestamp'] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
