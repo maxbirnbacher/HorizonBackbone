740b9e239e70a96bc0b548c7878396a0e0616fee
Maximilian <66524685+maxbirnbacher@users.noreply.github.com>
2024-02-25 14:33:20 +0100

    Update api.js

--- horizon-frontend/routes/api.js
+++ horizon-frontend/routes/api.js
@@ -159,11 +159,11 @@ router.get('/files/download/:fileID', function(req, res, next) {
         if (response.data) {
             res.send({
                 data: response.data,
-                filename: response.headers[content-disposition].split('filename=')[1], 
-                extension: response.headers[content-disposition].split('extension=')[1].split('.')[1],
+                filename: response.headers['content-disposition'].split('filename=')[1], 
+                extension: response.headers['content-disposition'].split('extension=')[1].split('.')[1],
             }, {
                 'Content-Type': 'application/octet-stream',
-                'Content-Disposition': 'attachment; filename=' + response.headers[content-disposition].split('filename=')[1]
+                'Content-Disposition': 'attachment; filename=' + response.headers['content-disposition'].split('filename=')[1]
             }, 200);
         } else {
             console.log('No data returned from API');
