7472804c36c8f995b7053108157d6f7c7335f3f5
Maximilian <66524685+maxbirnbacher@users.noreply.github.com>
2024-03-17 17:02:21 +0100

    trying to get env var to work

--- user-api/usermodel.py
+++ user-api/usermodel.py
@@ -52,5 +52,5 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     else:
         expire = datetime.utcnow() + timedelta(minutes=15)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, str(JWT_SECRET_KEY), algorithm=ALGORITHM)
     return encoded_jwt
