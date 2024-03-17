e3cbd47309eb44241c21239e5d11ab8df5e71fe8
Maximilian <66524685+maxbirnbacher@users.noreply.github.com>
2024-03-17 17:19:49 +0100

    Update login.js

--- horizon-frontend/public/javascripts/login.js
+++ horizon-frontend/public/javascripts/login.js
@@ -1,32 +1,34 @@
-// logic for login page
-document
-    .querySelector('#login-form')
-    .addEventListener('submit', function (event) {
-        event.preventDefault();
+document.addEventListener('DOMContentLoaded', () => {
+    // logic for login page
+    document
+        .querySelector('#login-form')
+        .addEventListener('submit', function (event) {
+            event.preventDefault();
 
-        var username = document
-            .querySelector('#login-demo-form-username')
-            .value;
-        var password = document
-            .querySelector('#login-demo-form-password')
-            .value;
+            var username = document
+                .querySelector('#login-demo-form-username')
+                .value;
+            var password = document
+                .querySelector('#login-demo-form-password')
+                .value;
 
-        // hash the password with sha256
-        var hashedPass = sha256(password.toString());
+            // hash the password with sha256
+            var hashedPass = sha256(password.toString());
 
-        axios
-            .post('http://localhost:3000/api/login', {
-                username: username,
-                password: hashedPass
-            })
-            .then(function (response) {
-                // Login successful, save the token
-                localStorage.setItem('token', response.data.access_token);
-                window.location.href = '/';
-            })
-            .catch(function (error) {
-                // Login failed
-                console.log('Login failed');
-                alert('Invalid username or password');
-            });
-    });
+            axios
+                .post('http://localhost:3000/api/login', {
+                    username: username,
+                    password: hashedPass
+                })
+                .then(function (response) {
+                    // Login successful, save the token
+                    localStorage.setItem('token', response.data.access_token);
+                    window.location.href = '/';
+                })
+                .catch(function (error) {
+                    // Login failed
+                    console.log('Login failed');
+                    alert('Invalid username or password');
+                });
+        });
+});
