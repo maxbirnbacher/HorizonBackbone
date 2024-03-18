aa557a32393920d332d303dc614ab4704a8a7fd8
Maximilian <66524685+maxbirnbacher@users.noreply.github.com>
2024-03-18 20:46:26 +0100

    Update login.js

--- horizon-frontend/public/javascripts/login.js
+++ horizon-frontend/public/javascripts/login.js
@@ -1,22 +1,24 @@
-function hashPassword(password) {
+async function hashPassword(password) {
     const encoder = new TextEncoder();
     const data = encoder.encode(password);
     try {
-        const hashBuffer = crypto
-            .subtle
-            .digest('SHA-256', data);
-            console.log('hashBuffer: ' + hashBuffer);
+        const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+        console.log('hashBuffer: ', hashBuffer);
         const hashArray = Array.from(new Uint8Array(hashBuffer));
-        console.log('hashArray: ' + hashArray);
+        console.log('hashArray: ', hashArray);
         const hashedPassword = hashArray
             .map(b => b.toString(16).padStart(2, '0'))
             .join('');
-            console.log('hashedPassword: ' + hashedPassword);
+        console.log('hashedPassword: ', hashedPassword);
         return hashedPassword;
     } catch (error) {
         console.error('Error hashing password:', error);
         return null;
     }
+}
+
+async function awaitEncryptPassword(password) {
+    return await hashPassword(password);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -34,29 +36,29 @@ document.addEventListener('DOMContentLoaded', () => {
                 .value;
 
             // hash the password with sha256
-            var hashedPass = hashPassword(password);
+            awaitEncryptPassword(password).then(hashedPass => {
+                console.log('username: ' + username);
+                console.log('password: ' + password);
+                console.log('hashedPass: ' + hashedPass);
 
-            console.log('username: ' + username);
-            console.log('password: ' + password);
-            console.log('hashedPass: ' + hashedPass);
-
-            fetch('/api/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({username: username, password: hashedPass})
-            })
-                .then(response => response.json())
-                .then(data => {
-                    // Login successful, save the token
-                    localStorage.setItem('token', data.access_token);
-                    window.location.href = '/';
+                fetch('/api/login', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({username: username, password: hashedPass})
                 })
-                .catch((error) => {
-                    // Login failed
-                    console.error('Login failed:', error);
-                    alert('Invalid username or password');
-                });
+                    .then(response => response.json())
+                    .then(data => {
+                        // Login successful, save the token
+                        localStorage.setItem('token', data.access_token);
+                        window.location.href = '/';
+                    })
+                    .catch((error) => {
+                        // Login failed
+                        console.error('Login failed:', error);
+                        alert('Invalid username or password');
+                    });
+            });
         });
 });
